2184ded4b7649dd899e0e918e19074296df074ac
MiniCreator21 <minianogamer21@gmail.com>
2025-09-25 14:12:13 +0100

    Finished empty world building
    Made it work and added edges to the cubes

--- Assets/Scripts/WorldHandler.cs
+++ Assets/Scripts/WorldHandler.cs
@@ -8,7 +8,7 @@ public class WorldHandler : MonoBehaviour
     #region Public Fields
     #endregion
     #region Private Variables
-    [SerializeField] private float pixelSize = 1.0f;
+    [SerializeField] private float pixelSize = 1f;
     private GameObject[,] worldPixels;
     private int[] worldSize = {8,8,8};
     #endregion
@@ -28,16 +28,21 @@ public class WorldHandler : MonoBehaviour
     #endregion
 
     #region World Creation
-    private void CreateWorld(int x, int y, int z){
-        float xPositionStart = -x / 2 - pixelSize / 2;
-        float yPositionStart = y / 2 + pixelSize / 2;
-        float zPositionStart = 0f;
+    private void CreateWorld(int x, int y, int z)
+    {
+        float pixelSizeOffset = pixelSize / 2;
+        float xPositionStart = -x / 2 - pixelSizeOffset;
+        float yPositionStart = y / 2 + pixelSizeOffset;
+        float zPositionStart = pixelSizeOffset;
         float xPosition = xPositionStart;
         float yPosition = yPositionStart;
         float zPosition = zPositionStart;
-        for (int i = 0; i < 2; i++){
-            for (int j = 0; j < y; j++){
-                for (int k = 0; k < x; k++){
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < y; j++)
+            {
+                for (int k = 0; k < x; k++)
+                {
                     Instantiate(worldPixel, new Vector3(xPosition, yPosition, zPosition), Quaternion.identity, parent: this.transform);
                     xPosition += pixelSize;
                 }
@@ -46,7 +51,47 @@ public class WorldHandler : MonoBehaviour
             }
             xPosition = xPositionStart;
             yPosition = yPositionStart;
-            zPosition += z * pixelSize;
+            zPosition += (z - 1) * pixelSize;
+        }
+        zPositionStart += pixelSize;
+        xPosition = xPositionStart;
+        yPosition = yPositionStart;
+        zPosition = zPositionStart;
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < y; j++)
+            {
+                for (int k = 0; k < z - 2; k++)
+                {
+                    Instantiate(worldPixel, new Vector3(xPosition, yPosition, zPosition), Quaternion.identity, parent: this.transform);
+                    zPosition += pixelSize;
+                }
+                zPosition = zPositionStart;
+                yPosition -= pixelSize;
+            }
+            zPosition = zPositionStart;
+            yPosition = yPositionStart;
+            xPosition += (x - 1) * pixelSize;
+        }
+        xPositionStart += pixelSize;
+        xPosition = xPositionStart;
+        yPosition = yPositionStart;
+        zPosition = zPositionStart;
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < x - 2; j++)
+            {
+                for (int k = 0; k < z - 2; k++)
+                {
+                    Instantiate(worldPixel, new Vector3(xPosition, yPosition, zPosition), Quaternion.identity, parent: this.transform);
+                    zPosition += pixelSize;
+                }
+                zPosition = zPositionStart;
+                xPosition += pixelSize;
+            }
+            zPosition = zPositionStart;
+            xPosition = xPositionStart;
+            yPosition -= (y - 1) * pixelSize;
         }
     }
     #endregion
