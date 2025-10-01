52ad560e3826773ad6c56c6f88616a0e9663b789
MiniCreator21 <minianogamer21@gmail.com>
2025-10-01 16:55:58 +0100

    added a seed system
    seed doesn't do anything yet

--- Assets/Scripts/WorldHandler.cs
+++ Assets/Scripts/WorldHandler.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class WorldHandler : MonoBehaviour
@@ -11,11 +12,13 @@ public class WorldHandler : MonoBehaviour
     [SerializeField] private float pixelSize = 1f;
     private GameObject[,] worldPixels;
     private int[] worldSize = {8,8,8};
+    private List<List<int>> worldSeed;
     #endregion
 
     #region Start
     void Start()
     {
+        CreateSeed();
         CreateWorld(worldSize[0], worldSize[1], worldSize[2]);
     }
     #endregion
@@ -24,6 +27,105 @@ public class WorldHandler : MonoBehaviour
     void Update()
     {
         
+    }
+    #endregion
+
+    #region Seed Creation
+    private void CreateSeed()
+    {
+        for (int i = 0; i < 2; i++) //iterates through front and back sides of cube 
+        {
+            if (i == 0)
+            {
+                List<int> newFace = new List<int>
+                {
+                    1,0,0,0,1,0,0,0,
+                    1,0,1,0,1,0,1,0,
+                    0,0,0,0,0,0,0,0,
+                    1,1,1,0,1,0,1,1,
+                    1,0,0,0,1,0,0,0,
+                    1,0,1,1,1,1,0,1,
+                    1,0,0,0,0,0,0,1,
+                    1,1,1,1,0,1,1,1
+                };
+            }
+            if (i == 1)
+            {
+                List<int> newFace = new List<int>
+                {
+                    0,1,0,0,0,0,0,0,
+                    0,0,0,0,0,0,1,0,
+                    0,0,0,0,0,0,0,0,
+                    0,0,0,0,0,1,0,0,
+                    0,0,0,0,0,0,0,1,
+                    0,0,0,0,1,0,0,0,
+                    0,0,1,0,1,0,0,0,
+                    1,0,1,0,1,0,0,0
+                };
+            }
+        }
+        for (int i = 0; i < 2; i++) //iterates through left and right sides of cube 
+        {
+            if (i == 0)
+            {
+                List<int> newFace = new List<int>
+                {
+                    1,1,1,1,1,1,1,1,
+                    0,0,0,0,0,0,0,1,
+                    0,1,0,0,0,0,0,1,
+                    0,0,0,1,1,0,1,0,
+                    1,0,0,1,0,0,1,0,
+                    0,0,0,0,0,1,1,0,
+                    0,0,1,1,0,1,0,0,
+                    1,1,1,1,1,1,0,0
+                };
+            }
+            if (i == 1)
+            {
+                List<int> newFace = new List<int>
+                {
+                    1,0,0,0,1,1,0,0,
+                    1,1,1,0,0,0,0,1,
+                    1,0,0,0,0,0,1,1,
+                    1,0,0,0,0,0,0,1,
+                    0,0,0,0,0,0,0,1,
+                    1,1,1,1,1,1,0,1,
+                    1,1,0,0,0,0,0,1,
+                    1,0,0,0,1,1,1,1
+                };
+            }
+        }
+        for (int i = 0; i < 2; i++) //iterates through top and bottom sides of cube 
+        {
+            if (i == 0)
+            {
+                List<int> newFace = new List<int>
+                {
+                    1,0,1,1,0,1,1,1,
+                    1,0,0,0,0,0,0,1,
+                    1,1,1,1,0,0,0,0,
+                    1,0,0,1,0,1,1,0,
+                    0,0,0,1,0,1,1,0,
+                    0,0,0,1,0,0,0,0,
+                    0,0,0,0,0,0,0,0,
+                    1,1,0,0,0,0,0,1
+                };
+            }
+            if (i == 1)
+            {
+                List<int> newFace = new List<int>
+                {
+                    1,1,0,0,0,0,0,1,
+                    1,0,0,0,0,0,0,0,
+                    1,0,0,1,1,0,0,0,
+                    1,0,0,1,1,1,0,0,
+                    1,0,0,1,1,0,0,0,
+                    1,0,0,0,0,0,0,0,
+                    1,0,0,0,0,0,1,1,
+                    1,1,1,0,1,1,1,1
+                };
+            }
+        }
     }
     #endregion
 
@@ -39,7 +141,7 @@ public class WorldHandler : MonoBehaviour
         float zPosition = zPositionStart;
         for (int i = 0; i < 2; i++)
         {
-            for (int j = 0; j < y; j++)
+            for (int j = 0; j < y; j++) 
             {
                 for (int k = 0; k < x; k++)
                 {
@@ -48,11 +150,11 @@ public class WorldHandler : MonoBehaviour
                 }
                 xPosition = xPositionStart;
                 yPosition -= pixelSize;
-            }
+            } //front face created 
             xPosition = xPositionStart;
             yPosition = yPositionStart;
             zPosition += (z - 1) * pixelSize;
-        }
+        } //back face created 
         zPositionStart += pixelSize;
         xPosition = xPositionStart;
         yPosition = yPositionStart;
@@ -68,11 +170,11 @@ public class WorldHandler : MonoBehaviour
                 }
                 zPosition = zPositionStart;
                 yPosition -= pixelSize;
-            }
+            } //left face created 
             zPosition = zPositionStart;
             yPosition = yPositionStart;
             xPosition += (x - 1) * pixelSize;
-        }
+        } //right face created 
         xPositionStart += pixelSize;
         xPosition = xPositionStart;
         yPosition = yPositionStart;
@@ -88,11 +190,11 @@ public class WorldHandler : MonoBehaviour
                 }
                 zPosition = zPositionStart;
                 xPosition += pixelSize;
-            }
+            } //top face created 
             zPosition = zPositionStart;
             xPosition = xPositionStart;
             yPosition -= (y - 1) * pixelSize;
-        }
+        } //bottom face created 
     }
     #endregion
 }
